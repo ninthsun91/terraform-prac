4a991b4137d9b6b0e33fc77eddbcdab31f6c0353
Ray Kim <ninthsun91@gmail.com>
2024-03-07 03:55:31 +0900

    environments - workspace init

---
+++ 5.environments/workspace/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  backend "s3" {
+    profile        = "terraform"
+    bucket         = "landing-tf-state"
+    key            = "practice/managing-multiple-environments/terraform.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-2"
+}
+
+variable "db_pass" {
+  description = "database password"
+  type        = string
+  sensitive   = true
+}
+
+locals {
+  environment_name = terraform.workspace
+}
+
+module "donut" {
+  source = "../../4.modules/web-app"
+
+  # Input Variables
+  app_name         = "donut-1"
+  environment_name = local.environment_name
+  create_dns_zone  = terraform.workspace == "production" ? true : false
+  domain           = "donut.place"
+  instance_type    = "t2.micro"
+  bucket_prefix    = "landingdonut-tf-practice-${local.environment_name}"
+  db_name          = "donut${local.environment_name}"
+  db_user          = "donut"
+  db_pass          = var.db_pass
+}
